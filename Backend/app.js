1235e7f8540918ed34e8593a90478cac862707ca
Renjith-38 <renjithphilip.mail@gmail.com>
2021-06-27 21:43:11 +0530

    Initial Commit

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = new express;
+
+
+
+const port = process.env.PORT || 3000;
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server listening on port:${port}`);
+})
